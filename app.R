bfec2d03d395f5e91501fc3c63f8080cc6730c14
Greg Maynard <CinderScript@gmail.com>
2023-03-15 18:33:20 -0700

    finished first 3 tabs with loading spinners

--- app.R
+++ app.R
@@ -1,13 +1,11 @@
 library(shiny)
-library(shinyjs)
 library(shinycssloaders)
-
 
 source("covid_study_data_plotter.R")
 
 
 vaccination_stat_choices <- list("Fully Vaccinated" = "vacc_complete_percent",
-                          "At Least One Dose" = "single_dose_percent")
+                                 "At Least One Dose" = "single_dose_percent")
 
 possible_axis_choices <- c(
   "Population Fully Vaccinated"   = "vacc_complete_percent",
@@ -21,213 +19,160 @@ possible_axis_choices <- c(
 ######### SHINY UI ########## 
 #############################
 
-ui <- fluidPage( useShinyjs(),
-
-    # Application title
-    titlePanel("Covid Vaccinations and their Effects on Healthcare"),
-
-    tabsetPanel(id = "tab", 
-                
-                h4("Select Statistic and Date to Visualize"),
-                
-                tabPanel(title = "Vaccination Levels Choropleth", value = "vaccination_choropleth_tab",
-                         sidebarPanel(selectInput("selected_map_vaccination_level",
-                                                  "Vaccination Level:",
-                                                  choices = vaccination_stat_choices, 
-                                                  selected = "vacc_complete_percent")),
-                         
-                         sidebarPanel(sliderInput("selected_vaccination_map_date",
-                                                  "Selected vaccination status date:",
-                                                  min = as.Date("2020-12-13", "%Y-%m-%d"),
-                                                  max = as.Date("2023-03-03", "%Y-%m-%d"),
-                                                  value = as.Date("2021/09/24"),
-                                                  timeFormat="%Y-%m-%d"))
-                ),
-                
-                tabPanel(title = "Vaccination and Hospitalization Plot", value = "vaccination_hospitalization_plot_tab",
-                         sidebarPanel(selectInput("selected_x_axis_stat",
-                                                  "Statistic to Plot on X-Axis:",
-                                                  choices = possible_axis_choices, 
-                                                  selected = "vacc_complete_percent")),
-                         
-                         sidebarPanel(selectInput("selected_y_axis_stat",
-                                                  "Statistic to Plot on Y-Axis:",
-                                                  choices = possible_axis_choices, 
-                                                  selected = "covid_bed_usage_ratio")),
-                         
-                         sidebarPanel(sliderInput("selected_vaccination_hospitalization_plot_date",
-                                                  "Select Date of Vaccination/Hospitalization Records:",
-                                                  min = as.Date("2020-12-13", "%Y-%m-%d"),
-                                                  max = as.Date("2023-03-03", "%Y-%m-%d"),
-                                                  value = as.Date("2021/09/24"),
-                                                  timeFormat="%Y-%m-%d"))
-                )
-    ),
-    
-    # Show our plot
-    mainPanel(
-        h2(textOutput("graph_title")),
-        h4("by Healthcare Referral Region"),
-        h4(htmlOutput("statistic_description")),
-
-        conditionalPanel(
-          condition = "input.plot_dynamic_toggle",
-          plotlyOutput("graph_dynamic") %>% withSpinner()),
-
-        conditionalPanel(
-          condition = "!input.plot_dynamic_toggle",
-          plotOutput("graph_static") %>% withSpinner()),
-
-        fluidRow(
-          column(3,checkboxInput("plot_dynamic_toggle", "Interactive Graph", F)), 
-          column(3,checkboxInput("is_scale_range_adaptive_toggle", "Adaptive Scale Range", F))),
-
-        hr(),
-        
-        p(htmlOutput("vacc_data_date")),
-        p(htmlOutput("bed_data_date")),
-        hr(),
-        p(tags$b("Program Messages:")),
-        p(htmlOutput("message"))
-        
-      )
+ui <- htmlTemplate( 
+  "app_ui.html",
+  
+  vacc_map_stat_select = selectInput( "vacc_map_stat",
+                                      "Vaccination Level:",
+                                      choices = vaccination_stat_choices, 
+                                      selected = "vacc_complete_percent"),
+  
+  vacc_map_date_select = sliderInput( "vacc_map_date",
+                                      "Select Date:",
+                                      min = as.Date("2020-12-13", "%Y-%m-%d"),
+                                      max = as.Date("2023-03-03", "%Y-%m-%d"),
+                                      value = as.Date("2021/09/24"),
+                                      timeFormat="%Y-%m-%d"),
+  
+  hosp_plot_x_select = selectInput( "selected_x_axis_stat",
+                                    "Show On X-Axis:",
+                                    choices = possible_axis_choices, 
+                                    selected = "vacc_complete_percent"),
+  
+  hosp_plot_y_select = selectInput( "selected_y_axis_stat",
+                                    "Show On Y-Axis:",
+                                    choices = possible_axis_choices, 
+                                    selected = "covid_bed_usage_ratio"),
+  
+  hosp_plot_date_select = sliderInput( "hosp_vacc_plot_date",
+                                       "Select Date::",
+                                       min = as.Date("2020-12-13", "%Y-%m-%d"),
+                                       max = as.Date("2023-03-03", "%Y-%m-%d"),
+                                       value = as.Date("2021/09/24"),
+                                       timeFormat="%Y-%m-%d"),
+  
+  graph = uiOutput("graph"),
+  
+  graph_title = textOutput("graph_title"),
+  graph_subtitle = htmlOutput("graph_subtitle"),
+  
+  vaccination_data_date = textOutput("vacc_data_date"),
+  hospitalization_data_date = textOutput("bed_data_date"),
+  
+  is_plot_dynamic = checkboxInput("is_plot_dynamic", "Interactive Graph", F),
+  is_scale_adaptive = checkboxInput("is_scale_adaptive", "Adaptive Scale", F)
 )
-
 
 ##################################
 ######### SHINY BACKEND ########## 
 ##################################
 
 server <- function(input, output, session) {
+  
+  ### Define reactive expressions to be used in several functions
+  ### For a function to trigger, a reactive value must be inside that function
+  vacc_map_date <- reactive(as.Date(input$vacc_map_date))
+  hosp_plot_date <- reactive(as.Date(input$hosp_vacc_plot_date))
+  vacc_stat <- reactive(input$vacc_map_stat)
+  x_axis_stat <- reactive(input$selected_x_axis_stat)
+  y_axis_stat <- reactive(input$selected_y_axis_stat)  
+  is_adaptive <- reactive(input$is_scale_adaptive)
+  active_tab <- reactive(input$active_tab)
+  
+  ### RENDER GRAPH
+  output$graph <- renderUI({
 
-  output$message <- renderUI({
+    #### to make renderUI trigger on changes of these controls
+    vacc_map_date()
+    hosp_plot_date()
+    vacc_stat()
+    x_axis_stat()
+    y_axis_stat()
+    is_adaptive()
+    active_tab()
     
-        "example text: Vaccination data succesfully loaded from argvis"
+    if (input$is_plot_dynamic) {
+      plotlyOutput("vacc_graph_dynamic") %>% withSpinner()
+    } else {
+      plotOutput("vacc_graph_static") %>% withSpinner()
+    }
   })
   
-  
-  ##### RENDER GRAPHS
-  output$graph_dynamic <- renderPlotly({
-    #date
-      is_choropleth_vacc_map = input$tab == "vaccination_choropleth_tab"
-      is_graph_dynamic = input$plot_dynamic_toggle
-      is_scale_range_adaptive = input$is_scale_range_adaptive_toggle
-    
-      selected_date <- ifelse(
-        is_choropleth_vacc_map, 
-        format(as.Date(input$selected_vaccination_map_date), "%Y-%m-%d"), 
-        format(as.Date(input$selected_vaccination_hospitalization_plot_date), "%Y-%m-%d"))
+  output$vacc_graph_dynamic <- renderPlotly(
+    {
+      req(active_tab())
       
-    # statistic 1
-      selected_x_axis <- ifelse(
-        is_choropleth_vacc_map, 
-        input$selected_map_vaccination_level, 
-        input$selected_x_axis_stat)
-    
-    #statistic 2
-      selected_y_axis <- input$selected_y_axis_stat
-      
-    # Show / Hide
-      toggleState("is_scale_range_adaptive_toggle", condition = is_choropleth_vacc_map)
-      
-      if (is_graph_dynamic) {         #Plot Selected Graph - dynamic
-        
-          ### Graph Choropleth
-          if (is_choropleth_vacc_map)
-            graph_plotly_vacc_choropleth(
-              Graph_Vaccination_Rates_Choropleth_By_Hrr(
-                selected_date, 
-                display_stat = selected_x_axis,
-                is_scale_range_adaptive))
-          
-          ### Else Graph Point Plot
-          else
-            graph_plotly_point_plot(
-              Graph_Vaccination_Hospitalization_Plot(
-                selected_date, selected_x_axis, selected_y_axis))
+      if (active_tab() == "nav_vacc_map_tab"){
+        graph_plotly_vacc_choropleth(                 # need to turn it into a plotly object
+          Graph_Vaccination_Rates_Choropleth_By_Hrr(
+            date = as.Date(vacc_map_date()), 
+            display_stat = vacc_stat(),
+            is_scale_adaptive = is_adaptive()))
       }
-      else {
-          return(NULL)
+      else if(input$active_tab == "nav_hosp_plot_tab"){
+        graph_plotly_point_plot(                 # need to turn it into a plotly object
+          Graph_Vaccination_Hospitalization_Plot(
+            date = as.Date(hosp_plot_date()), 
+            x_axis = x_axis_stat(),
+            y_axis = y_axis_stat()))
       }
-
-  })
+    })
   
-  output$graph_static <- renderPlot({
-    
-    is_choropleth_vacc_map = input$tab == "vaccination_choropleth_tab"
-    is_graph_dynamic = input$plot_dynamic_toggle
-    is_scale_range_adaptive = input$is_scale_range_adaptive_toggle
-    
-    #date
-    selected_date <- ifelse(
-      is_choropleth_vacc_map, 
-      format(as.Date(input$selected_vaccination_map_date), "%Y-%m-%d"), 
-      format(as.Date(input$selected_vaccination_hospitalization_plot_date), "%Y-%m-%d"))
-    
-    # statistic 1
-    selected_x_axis <- ifelse(
-      is_choropleth_vacc_map, 
-      input$selected_map_vaccination_level, 
-      input$selected_x_axis_stat)
-    
-    #statistic 2
-    selected_y_axis <- input$selected_y_axis_stat
-    
-    # Show / Hide
-    toggleState("is_scale_range_adaptive_toggle", condition = is_choropleth_vacc_map)
-    
-    if (!is_graph_dynamic) {         #Plot Selected Graph - static
+  output$vacc_graph_static <- renderPlot(
+    {
+      req(active_tab())
       
-      ### Graph Choropleth
-      if (is_choropleth_vacc_map)
+      if (active_tab() == "nav_vacc_map_tab"){
         Graph_Vaccination_Rates_Choropleth_By_Hrr_Static(
-            selected_date, 
-            display_stat = selected_x_axis,
-            is_scale_range_adaptive)
-      
-      ### Else Graph Point Plot
-      else
+          date = as.Date(vacc_map_date()), 
+          display_stat = vacc_stat(),
+          is_scale_adaptive = is_adaptive())
+      }
+      else if(active_tab() == "nav_hosp_plot_tab"){
+        print("GRAPHING HOSP PLOT!")
+        print(paste("X =", x_axis_stat()))
+        print(paste("Y =", y_axis_stat()))
         Graph_Vaccination_Hospitalization_Plot_Static(
-            selected_date, selected_x_axis, selected_y_axis)
-    }
-    else {
-      NULL
-    }
-  })
+          date = as.Date(hosp_plot_date()), 
+          x_axis = x_axis_stat(),
+          y_axis = y_axis_stat())
+      }
+    })
   
   
   #### RENDER TITLES
   output$graph_title <- renderText({
-    ifelse (input$tab == "vaccination_choropleth_tab",
+    ifelse (active_tab() == "nav_vacc_map_tab",
             "Covid Vaccination Rates in the USA",
             "Covid Vaccination and Hospital Strain")
   })
   
-  output$statistic_description <- renderUI({
-      text = ""
-      if (input$tab == "vaccination_choropleth_tab") {
-          stat = possible_axis_labels[input$selected_map_vaccination_level]
-          text = paste("HHR population statistic:", stat)
-      }
-      
-      text
-  })
-  
-  #### RENDER GRAPH INFO (bottom)
-  output$vacc_data_date <- renderUI({
-    valid_data_dates = closest_valid_dates(input$selected_vaccination_map_date)
-    em(paste("Vaccination data from:", valid_data_dates[1]))
-  })
-  output$bed_data_date <- renderUI({
-    
-    if (input$tab == "vaccination_choropleth_tab") {
-        return("")
+  output$graph_subtitle <- renderText({
+    req(active_tab())
+    req(vacc_stat())
+    if (active_tab() == "nav_vacc_map_tab") {
+      stat = possible_axis_labels[vacc_stat()]
+      text = paste("Population Vaccination Level:", stat)
     }
-    
-    valid_data_dates = closest_valid_dates(input$selected_vaccination_map_date)
-    em(paste("Hospital bed usage data from:", valid_data_dates[2]))
   })
   
+  #### RENDER DATE OF DATASET (bottom)
+  output$vacc_data_date <- renderText({
+    req(active_tab())
+    date_text = "Date of Vaccination Records: "
+    
+    if(active_tab() == "nav_vacc_map_tab")
+      date_text = paste(date_text, closest_valid_dates(vacc_map_date())[1])
+    else
+      date_text = paste(date_text, closest_valid_dates(hosp_plot_date())[1])
+  })
+  output$bed_data_date <- renderText({
+    req(active_tab())
+    if(active_tab() == "nav_hosp_plot_tab")
+      paste(
+        "Date of Hospital Records: ",
+        closest_valid_dates(hosp_plot_date())[2])
+  })
 }
 
 # Run the application 
--- www/app_ui.js
+++ www/app_ui.js
@@ -1,19 +1,54 @@
 
 // Trigger the click event on the default active tab after the Shiny app is loaded
 // so that renderPlot or renderPlotly have the value of the selected tab on startup
+// CLICK THE DEFAULT TAB ON STARTUP
 $(document).on('shiny:connected', function () {
-    $('#nav_hosp_plot_tab').trigger('click');
+    $('#nav_vacc_map_tab').trigger('click');
 });
 
-// Keep track of the current tab
+last_tab_state = "nav_vacc_map_tab"
+remembered_checkbox_state = false
+
+function remember_checkbox_checked_state(){
+    remembered_checkbox_state = document.getElementById('is_scale_adaptive').checked
+}
+function set_adaptive_checkbox_enabled(state){
+    document.getElementById('is_scale_adaptive').disabled = !state
+}
+function set_checkbox_checked_state(checked) {
+    return document.getElementById('is_scale_adaptive').checked = checked
+}
+
+// Keep track of the current tab and the state of the adaptive scale checkbox
 document.getElementById('nav_vacc_map_tab').addEventListener('click', function () {
+
+    // Disable and Enable "Scale Adaptive" checkbox
+    set_adaptive_checkbox_enabled(true)
+    set_checkbox_checked_state(remembered_checkbox_state)
+
     Shiny.setInputValue('active_tab', 'nav_vacc_map_tab');
+    last_tab_state = "nav_vacc_map_tab"
 });
 
 document.getElementById('nav_hosp_plot_tab').addEventListener('click', function () {
+    if (last_tab_state = "nav_vacc_map_tab")
+        remember_checkbox_checked_state()
+
+    set_adaptive_checkbox_enabled(false)
+    set_checkbox_checked_state(true)
     Shiny.setInputValue('active_tab', 'nav_hosp_plot_tab');
+    last_tab_state = "nav_hosp_plot_tab"
 });
 
 document.getElementById('nav_about_tab').addEventListener('click', function () {
+    if (last_tab_state = "nav_vacc_map_tab")
+        remember_checkbox_checked_state()
+
+    set_checkbox_checked_state(false)
+    set_adaptive_checkbox_enabled(false)
     Shiny.setInputValue('active_tab', 'nav_about_tab');
+    last_tab_state = "nav_about_tab"
 });
+
+
+
